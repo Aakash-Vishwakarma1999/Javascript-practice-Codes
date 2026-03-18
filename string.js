fb5a35fd64a1c21972b023953d2bf16e1e98106d
aakashrv19999-tech <aakashrv19999@gmail.com>
2026-03-18 23:56:32 +0530

    String codes

---
+++ string.js
@@ -0,0 +1,24 @@
+
+// Index:  0 1 2 3 4 5 6 7 8 9 10 11 12 13
+// Chars:  T h i s   i s   a     t  e  s  t
+let str= "This is a test";
+console.log(str.indexOf("is"));
+console.log(str.lastIndexOf("is"));
+
+
+// Index:  0 1 2 3 4 5 6 7 8 9 10 11 12 13
+// Chars:  T h i s   i s   a     t  e  s  t
+let str= "This is a test";
+console.log(str.indexOf("is",5));
+console.log(str.lastIndexOf("is",4));
+
+
+
+//Index:  0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+//Chars:  F i r s t   S e c o  n  d     T  h  i  r d     f  o  u  r  t   h
+let str="First Second Third Fourth";
+console.log(str.slice(10));
+console.log(str.slice(-10)); //starts from left to right and counts once 10 finishes
+
+
+
